c35b5c8ee49b4541bd985b7b40377d97b9b35465
10xMike <michelegec@gmail.com>
2021-04-25 19:04:53 +0200

    Sistemati gli elementi in HTML

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,31 @@
 // chiedere all'utente il numero di chilometri che vuole percorrere
     var km = prompt('Quanti kilometri vuoi percorrere?');
     console.log(km);
+    document.getElementById('km').innerHTML = "La distanza in km è di " + km;
 // l'età del passeggero
     var age = prompt('Quanti anni hai?');
     console.log(age);
+    document.getElementById('age').innerHTML = "La tua età è " + age + " anni";
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
     var rate = 0.21;
     var price = km * rate;
     console.log(price);
 // va applicato uno sconto del 20% per i minorenni    
     var minorsDiscount = ((price * 20 ) / 100);
+    console.log(minorsDiscount);
+
+    if (age <= 17) {
+        document.getElementById('minorsdiscount').innerHTML = "Hai diritto allo sconto per minorenni"
+    }
 // va applicato uno sconto del 40% per gli over 65
-    var seniorsDiscount = ((price * 40) / 100); 
-    var finalprice = price - seniorsDiscount;
+    var seniorsDiscount = ((price * 40) / 100);
+    console.log(seniorsDiscount); 
+
+    if (age > 65) {
+        document.getElementById('seniorsdiscount').innerHTML = "Hai diritto allo sconto per anziani"
+    }
+// Calcolo
+
     if (age <= 17) {
         finalprice = price - minorsDiscount;
     } else if (age > 65) {
