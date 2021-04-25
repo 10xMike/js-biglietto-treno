b189ea1105178ea34328335f99ce688a424c3441
10xMike <michelegec@gmail.com>
2021-04-25 19:08:59 +0200

    Commit finale

--- assets/main.js
+++ assets/main.js
@@ -2,14 +2,18 @@
     var km = prompt('Quanti kilometri vuoi percorrere?');
     console.log(km);
     document.getElementById('km').innerHTML = "La distanza in km è di " + km;
+
 // l'età del passeggero
     var age = prompt('Quanti anni hai?');
     console.log(age);
     document.getElementById('age').innerHTML = "La tua età è " + age + " anni";
+
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
     var rate = 0.21;
     var price = km * rate;
     console.log(price);
+    document.getElementById('price').innerHTML = "Il prezzo iniziale è di " + price;
+    
 // va applicato uno sconto del 20% per i minorenni    
     var minorsDiscount = ((price * 20 ) / 100);
     console.log(minorsDiscount);
@@ -18,6 +22,7 @@
         document.getElementById('minorsdiscount').innerHTML = "Hai diritto allo sconto per minorenni"
     }
 // va applicato uno sconto del 40% per gli over 65
+
     var seniorsDiscount = ((price * 40) / 100);
     console.log(seniorsDiscount); 
 
@@ -34,6 +39,7 @@
         finalprice = price;
     }
 // L'output del prezzo finale va messo con massimo due decimali, per indicare centesimi sul prezzo
+
     var finalpricerounded = Math.round(finalprice * 100) / 100;
     console.log(finalpricerounded);
-    document.getElementById('finalpricerounded').innerHTML = "Il prezzo del tuo biglietto è di " + finalpricerounded + "€";
+    document.getElementById('finalpricerounded').innerHTML = "Il prezzo finale del tuo biglietto è di " + finalpricerounded + "€";
